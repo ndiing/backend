108149ce6853b9d77ef59f8ea8359a1984b9e9e8
Ridho Prasetya <ndiing.inc@gmail.com>
2024-10-06 10:50:30 +0700

    save

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,14 +1,25 @@
-// require("./cert.js");
 require("./config.js");
+
+// pindah ke @ndiinginc/cert
+// require("./cert.js");
+
+// pindah ke @ndiinginc/fetch
 // require("./cookie-store.js");
 // require("./fetch.js");
 // require("./headers.js");
 // require("./helper.js");
-// require("./jwt.js");
-// require("./middleware.js");
-// require("./object-observer.js");
-// require("./otp.js");
-// require("./request.js");
 // require("./response.js");
+// require("./request.js");
+// require("./object-observer.js");
+
+// pindah ke @ndiinginc/jwt
+// require("./jwt.js");
+
+// pindah ke @ndiinginc/otp
+// require("./otp.js");
+
+// pindah ke @ndiinginc/router
 // require("./router.js");
+// require("./middleware.js");
+
 require("./validation.js");

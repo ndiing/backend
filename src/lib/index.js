e387733a933f37e4760b2cb810b05aace46ceb8c
Ridho Prasetya <ndiing.inc@gmail.com>
2024-10-06 23:09:52 +0700

    commit

--- src/lib/index.js
+++ src/lib/index.js
@@ -24,3 +24,88 @@
 // require("./middleware.js");
 
 // require("./validation.js");
+
+// {
+//     const fs = require("fs");
+//     const path = require("path");
+
+//     function getCertsForHostname(domain = "localhost") {
+//         let key;
+//         let cert;
+
+//         try {
+//             key = fs.readFileSync(path.join(process.cwd(), "host.key"));
+//             cert = fs.readFileSync(path.join(process.cwd(), "host.crt"));
+//         } catch (error) {
+//             const root = generateRootCA();
+//             const host = generateCertsForHostname(domain, {
+//                 key: root.privateKey,
+//                 cert: root.certificate,
+//             });
+
+//             fs.writeFileSync(path.join(process.cwd(), "root.key"), root.privateKey);
+//             fs.writeFileSync(path.join(process.cwd(), "root.crt"), root.certificate);
+
+//             const dirname = path.join(process.cwd());
+//             const filename = path.join(process.cwd(), "root.crt");
+
+//             execSync(`powershell -Command "Start-Process cmd -Verb RunAs -ArgumentList '/c cd ${dirname} && certutil -enterprise -addstore -f root ${filename}'"`, { shell: true });
+
+//             key = fs.writeFileSync(path.join(process.cwd(), "host.key"), host.privateKey);
+//             cert = fs.writeFileSync(path.join(process.cwd(), "host.crt"), host.certificate);
+//         }
+
+//         return {
+//             key,
+//             cert,
+//         };
+//     }
+// }
+
+// {
+//     const puppeteer = require("puppeteer-core"); // v23.0.0 or later
+
+//     (async () => {
+//         const browser = await puppeteer.launch({
+//             executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+//             args: [
+//                 //
+//                 "--window-position=-999999,-999999",
+//             ],
+//         });
+//         const page = await browser.newPage();
+
+//         await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36')
+//         await page.evaluateOnNewDocument(() => {
+//             delete navigator.__proto__.webdriver
+//         })
+
+//         const timeout = 5000;
+//         page.setDefaultTimeout(timeout);
+
+//         {
+//             const targetPage = page;
+//             await targetPage.setViewport({
+//                 width: 1063,
+//                 height: 736,
+//             });
+//         }
+//         {
+//             const targetPage = page;
+//             const promises = [];
+//             const startWaitingForEvents = () => {
+//                 promises.push(targetPage.waitForNavigation());
+//             };
+//             startWaitingForEvents();
+//             await targetPage.goto("https://bot.sannysoft.com/");
+//             await Promise.all(promises);
+//         }
+
+//         await page.screenshot({path:'screenshot.png',fullPage:true});
+
+//         await browser.close();
+//     })().catch((err) => {
+//         console.error(err);
+//         process.exit(1);
+//     });
+// }
